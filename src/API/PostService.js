d66c903544d5377f79da57161eb5363ad59be8d2
zerad11 <iooin@bk.ru>
2024-02-07 19:41:31 +0300

    test

---
+++ src/API/PostService.js
@@ -0,0 +1,25 @@
+// это пример
+// import axios from "axios";
+
+// export default class PostService {
+//     static async getAll(limit = 10, page = 1) {
+//         const url = 'https://jsonplaceholder.typicode.com'
+//         const response = await axios.get(`${url}/posts`, {
+//             params: {
+//                 _limit: limit,
+//                 _page: page
+//             }
+//         })
+//         return response;
+//     }
+
+//     static async getById(id) {
+//         const response = await axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+//         return response;
+//     }
+
+//     static async getCommentsByPostId(id) {
+//         const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+//         return response;
+//     }
+// }
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <b>ВСЕ РАБОТАЕТ</b>
+        </div>
+    )
+}
+
+export default App;
