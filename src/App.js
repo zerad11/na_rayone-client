d06544c7959eb1c007cb2d9aa53eca1c9a580e49
Kruchinkin Sergey <kruchinkin.s@yandex.ru>
2024-02-08 22:52:22 +0300

    add component categories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './styles/App.css';
-import axios from 'axios';
 import { Categories } from './components/Categories';
 
 function App() {
-
     return (
         <div className='App'>
             <Categories/>
